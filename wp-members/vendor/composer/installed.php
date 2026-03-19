ef5d553e5a7c581f4b7fa7856bca4bab6b8e7d9e
EC <casianoemmanuel2217@gmail.com>
2026-03-19 23:53:18 +0800

    Initial commit

---
+++ wp-members/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yourname/wp-members',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a1d682aa9a50b4350edb13c18ee1972dc4509dc7',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yourname/wp-members' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a1d682aa9a50b4350edb13c18ee1972dc4509dc7',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
